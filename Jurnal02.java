d17d554b21c30ea22927c196f1152923fd188a47
unknown <pradiptapryanda301203@gmail.com>
2021-10-01 14:26:43 +0700

    Jurnal02

---
+++ Jurnal02.java
@@ -0,0 +1,9 @@
+public class Jurnal02 {
+	public static void main(String[] args) {
+	    System.out.println("NIM  : 6706213093");
+		System.out.println("Nama : Pradipta Deska Pryanda");
+		System.out.println("Kelas: D3IF-45-04");
+		
+	
+	}
+}
