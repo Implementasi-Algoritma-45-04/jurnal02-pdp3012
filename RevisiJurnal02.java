539b7cb7115b701534d9548eac5a18478130abd6
unknown <pradiptapryanda301203@gmail.com>
2021-10-03 10:59:17 +0700

    RevisiJurnal02

---
+++ RevisiJurnal02.java
@@ -0,0 +1,8 @@
+public class RevisiJurnal02 {
+
+    public static void main(String[] args) {
+	    System.out.println("NAMA : PRADIPTA DESKA PRYANDA");
+	    System.out.println("NIM  : 6706213093");
+	    System.out.println("KELAS: D3IF 45-04");
+	}
+}	
